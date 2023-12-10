51c20ce198049eeebec49ad38defa4963fe1d8a5
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-10 21:47:38 +0300

    Remove unneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout";
 import HomePage from "./Components/HomePage";
 import SignUp from "./Components/SignUp";
+import SignIn from "./Components/SignIn";
+import Payment from "./Components/Payment";
+import PaymentsReport from "./Components/PaymentsReport";
+import Policy from "./Components/Policy";
+import LearnMore from "./Components/LearnMore";
 import Main from "./Components/Main";
-// import "./App.css";
 
 function App() {
   return (
@@ -14,6 +18,11 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/Main" element={<Main />} />
           <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/Payment" element={<Payment />} />
+          <Route path="/PaymentsReport" element={<PaymentsReport />} />
+          <Route path="/Policy" element={<Policy />} />
+          <Route path="/LearnMore" element={<LearnMore />} />
         </Routes>
       </Layout>
     </Router>
