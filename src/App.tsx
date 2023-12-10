13f0014cf92dbb1e5043b674c759b3a5b85cfafb
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-10 20:47:22 +0300

    Fix issues in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Layout from "./Components/Layout"; // Import the Layout component
+import HomePage from "./Components/HomePage"; // Import the HomePage component
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {/* Layout will automatically include Nav and Footer */}
+      <HomePage /> {/* This is where you render the homepage content */}
+    </Layout>
   );
 }
 
