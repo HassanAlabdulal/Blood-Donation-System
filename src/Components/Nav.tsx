863e041fbaf3b22dd7d5269a7da30172ed18272d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-10 21:03:40 +0300

    link the nav with the pages

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 // import ProfileMenu from "../components/UI/ProfileMenu";
 
 const navMotion = {
@@ -43,13 +45,9 @@ const NavLinks = ({
 }) => (
   <div className={className}>
     {navLinks.map(({ name, href, id }) => (
-      <motion.a
-        key={id}
-        variants={isMobile ? itemMotion : itemMotionDesktop}
-        href={href}
-      >
-        {name}
-      </motion.a>
+      <motion.div key={id} variants={isMobile ? itemMotion : itemMotionDesktop}>
+        <Link to={href}>{name}</Link>
+      </motion.div>
     ))}
   </div>
 );
