1f7019793be1f22b0b33a878d69b17f65559ea4d
HassanAlabdulal <hhhalabdulaal@gmail.com>
2023-12-10 23:44:04 +0300

    Fix linking issue in the footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,40 +1,42 @@
+import { Link } from "react-router-dom";
+
 export default function Footer() {
   return (
     <footer className=" bg-[#f7f7f7] rounded-lg shadow">
       <div className="w-full max-w-screen-xl p-4 mx-auto md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="flex items-center mb-4 space-x-3 sm:mb-0 rtl:space-x-reverse"
           >
             <span className="self-center text-2xl font-semibold text-red-700 whitespace-nowrap">
               DonorHub
             </span>
-          </a>
+          </Link>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0">
             <li>
-              <a href="AboutUsPage" className="hover:underline me-4 md:me-6">
+              <Link to="/AboutUs" className="hover:underline me-4 md:me-6">
                 About us
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="PolicyPage" className="hover:underline me-4 md:me-6">
+              <Link to="/Policy" className="hover:underline me-4 md:me-6">
                 Privacy Policy
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="ContactUsPage" className="hover:underline">
+              <Link to="/ContactUs" className="hover:underline">
                 Contact us
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center ">
           © 2023{" "}
-          <a href="/" className="hover:underline">
+          <Link to="/" className="hover:underline">
             DonorHub™
-          </a>
+          </Link>
           . All Rights Reserved.
         </span>
       </div>
