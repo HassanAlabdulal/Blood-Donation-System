da11918c94edfff67df913125b6b60354869d0d0
7ussainlz <hussainlutfi@hotmail.com>
2023-12-20 12:51:47 +0300

    LAST

--- src/Components/EditProfileAdmin.tsx
+++ src/Components/EditProfileAdmin.tsx
@@ -1,147 +1,126 @@
 import React, { useState, useEffect } from "react";
-import { useParams  } from "react-router-dom";
 import { supabase } from "../utils/supabase";
+import { Link , useNavigate, useParams  } from "react-router-dom";
+import { User } from "@supabase/supabase-js";
 
-  // Provided user profile data
-  type userProfile = {
-    userId: string;
-    name: string;
-    email: string;
-    phoneNumber: string;
-    bloodType: string;
-    dateOfBirth: string;
-    age: number;
-    weight: number;
-    country: string;
-    city: string;
-    street: string;
-    postalCode: string;
-    medicalHistory: string;
-  };
+type userProfile = {
+  userId: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  bloodType: string;
+  dateOfBirth: string;
+  age: number;
+  weight: number;
+  country: string;
+  city: string;
+  street: string;
+  postalCode: string;
+  medicalHistory: string;
+};
   
   export default function EditProfileAdmin() {
     // Provided user profile data
     const params = useParams();
-    const [ diseases , setDiseases] = useState("")
-    const [userProfile, setUserProfile] = useState<userProfile >(
-      {
-        userId: "LOADING...",
-        name: "LOADING...",
-        email: "LOADING...",
-        phoneNumber: "LOADING...",
-        bloodType: "LOADING...",
-        dateOfBirth: Date.now().toString(),
-        age: 0,
-        weight: 0,
-        country: "LOADING...",
-        city: "LOADING...",
-        street: "LOADING...",
-        postalCode: "LOADING...",
-        medicalHistory: "LOADING...",
-      }
-    )
-  
-    // Example user profile data
-    useEffect(() => {
-      // console.log(params.id)
-      getMedicalHistory();
-      getProfile();
-    });
-  
-    
-  
-    const getProfile = async () => {
-      const { data, error } = await supabase
-        .from("profiles")
-        .select()
-        .eq("id", params.id);
-  
-        if (data){
-          setUserProfile({
-            userId: data[0].id,
-            name: data[0].full_name,
-            email: data[0].username,
-            phoneNumber: data[0].phone,
-            bloodType: data[0].bloodType,
-            dateOfBirth: data[0].DoB,
-            age: age,
-            weight: data[0].weight,
-            country: data[0].country,
-            city: data[0].city,
-            street: data[0].street,
-            postalCode: data[0].postalCode,            medicalHistory:diseases,
-          })
-          const today = new Date();
-          const birthDate = new Date(data[0].DoB);
-          let age_now = today.getFullYear() - birthDate.getFullYear();
-          const m = today.getMonth() - birthDate.getMonth();
-          if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-            age_now--;
-          }
-          setAge(age_now);
-        }
-        
-      }
-  
-      const getMedicalHistory = async () => {
-        const { data, error } = await supabase
-        .from('MedicalHistory')
-        .select()
-        .eq('patientID', params.id)
-    
-        if(data) {getDiseases()}
-        else {setDiseases("Nothing")}
-      }
-      const getDiseases =async () => {
-        const { data, error } = await supabase
-        .from('Diseases')
-        .select()
-        .eq('medicalPID', params.id)
-    
-        if(data) {
-          setDiseases(data.map(d => d.Disease).toString())
-        }
-        else {setDiseases("Nothing")}
-      }
+    const navigate = useNavigate();
+  // const [user, setUser] = useState<User | null>(null);
+  const [ diseases , setDiseases] = useState<string[]>()
+  const [userProfile, setUserProfile] = useState<userProfile >(
+    {
+      userId: "LOADING...",
+      name: "LOADING...",
+      email: "LOADING...",
+      phoneNumber: "LOADING...",
+      bloodType: "LOADING...",
+      dateOfBirth: "LOADING...",
+      age: 0,
+      weight: 0,
+      country: "LOADING...",
+      city: "LOADING...",
+      street: "LOADING...",
+      postalCode: "LOADING...",
+      medicalHistory: "LOADING...",
+    }
+  )
 
-  // State variables initialized with userProfile data
-  const [userId, setUserId] = useState(userProfile.userId);
-  const [name, setName] = useState(userProfile.name);
-  const [email, setEmail] = useState(userProfile.email);
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [bloodType, setBloodType] = useState(userProfile.bloodType);
-  const [dateOfBirth, setDateOfBirth] = useState(userProfile.dateOfBirth);
+  // Example user profile data
+  useEffect(() => {
+    // getUserProfile()
+    getMedicalHistory();
+    getProfile();    
+  });
+
+
+
+  
+
+  const getProfile = async () => {
+    const { data, error } = await supabase
+      .from("profiles")
+      .select()
+      .eq("id", params.id);
+
+      if (data){
+        setUserProfile({
+          userId: data[0].id,
+          name: data[0].full_name,
+          email: data[0].username,
+          phoneNumber: data[0].phone,
+          bloodType: data[0].bloodType,
+          dateOfBirth: data[0].DoB,
+          age: age,
+          weight: data[0].weight,
+          country: data[0].country,
+          city: data[0].city,
+          street: data[0].street,
+          postalCode: data[0].postalCode,
+          medicalHistory:diseases?.toString()!,
+        })
+        const today = new Date();
+        const birthDate = new Date(data[0].DoB);
+        let age_now = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+          age_now--;
+        }
+        setAge(age_now);
+        }
+    }
+
+    const getMedicalHistory = async () => {
+      const { data, error } = await supabase
+      .from('MedicalHistory')
+      .select()
+      .eq('patientID', params.id)
+  
+      if(data) {getDiseases()}
+      else {setDiseases(["Nothing"])}
+    }
+    const getDiseases =async () => {
+      const { data, error } = await supabase
+      .from('Diseases')
+      .select()
+      .eq('medicalPID', params.id)
+  
+      if(data) {
+        setDiseases(data.map(d => d.Disease))
+        // getDiseasesPreventingDonation()
+      }
+      else {setDiseases(["Nothing"])}
+    }
+  // State variables for editable fields
+  const [phoneNumber, setPhoneNumber] = useState<string>();
   const [age, setAge] = useState<number>(0);
-  const [weight, setWeight] = useState(userProfile.weight);
+  const [weight, setWeight] = useState<number>();
   const [country, setCountry] = useState<string>();
   const [city, setCity] = useState<string>();
   const [street, setStreet] = useState<string>();
-  const [pCode, setPCode] = useState<string>();  const [medicalHistory, setMedicalHistory] = useState(
+  const [pCode, setPCode] = useState<string>();
+  const [name, setName] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [medicalHistory, setMedicalHistory] = useState(
     userProfile.medicalHistory
   );
- 
-  // useEffect(() => {
-  //   // Recalculate age whenever the date of birth changes
-  //   const calculateAge = (dob: string | number | Date): number => {
-  //     const today = new Date();
-  //     const birthDate = new Date(dob);
-  //     let age_now = today.getFullYear() - birthDate.getFullYear();
-  //     const m = today.getMonth() - birthDate.getMonth();
-  //     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-  //       age_now--;
-  //     }
-  //     return age_now;
-  //   };
-
-  //   setAge(calculateAge(dateOfBirth));
-  // }, [dateOfBirth]);
-
-  const getMaxDate = () => {
-    const today = new Date();
-    return today.toISOString().split("T")[0];
-  };
-
-  const bloodTypes = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
 
   const [diseasesPreventingDonation, setDiseasesPreventingDonation] = useState([
     "-",
@@ -155,19 +134,64 @@ import { supabase } from "../utils/supabase";
     "HIV/AIDS" ,
     "Malaria" ,
   ])
+  
+
+  const bloodTypes = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
 
 
+  const handleSave = async () => {
+    const { data, error  } = await supabase
+    .from('profiles')
+    .update({
+      phone: phoneNumber,
+      weight: weight,
+      country,
+      city,
+      street,
+      postalCode: pCode,
+    })
+    .eq('id', params.id)
+    .select()
+    if ( medicalHistory !==  "-"){
+      const {data , error} = await supabase
+      .from('MedicalHistory')
+      .select()
+      .eq('patientID', params.id)
+
+      if (data){ if(data.length>0){
+        const { error }= await supabase
+        .from('Diseases')
+        .insert([{
+          medicalPID: params.id,
+          Disease: medicalHistory
+        }])
+      }
+    else{
+      const {error} = await supabase
+      .from('MedicalHistory')
+      .insert([{patientID: params.id}])
+
+      if (!error) {const { error }= await supabase
+        .from('Diseases')
+        .insert([{
+          medicalPID: params.id,
+          Disease: medicalHistory
+        }])
+}
+
+    }}
+    }
+    // navigate("/ShowProfile");
+    // window.location.reload();
+  }
 
   return (
     <div className="bg-[#f7f7f7] pt-16 flex flex-col justify-center w-full items-center min-h-screen font-roboto">
       <div className="flex flex-col items-center w-2/3 max-w-4xl p-8 space-y-8 overflow-hidden bg-white rounded-lg shadow-lg">
-        {/* Title */}
         <h1 className="text-2xl font-semibold text-gray-900">Edit Profile</h1>
-
-        {/* Form for User Details */}
         <div className="w-full">
-          <form className="space-y-4">
-            {/* User ID */}
+          <form className="space-y-4" onSubmit={handleSave}>
+            {/* Fixed Fields */}
             <div>
               <label
                 className="block text-sm font-medium text-gray-700"
@@ -176,16 +200,14 @@ import { supabase } from "../utils/supabase";
                 User ID
               </label>
               <input
-                type="text"
-                className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md"
                 id="user-id"
-                
-                placeholder={userId}
+                type="text"
+                value={userProfile.userId}
                 disabled
               />
             </div>
 
-            {/* Combined Name Field */}
             <div>
               <label
                 className="block text-sm font-medium text-gray-700"
@@ -194,12 +216,13 @@ import { supabase } from "../utils/supabase";
                 Name
               </label>
               <input
-                className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md"
                 id="name"
                 type="text"
+                placeholder={userProfile.name}
                 value={name}
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Name"
+                onChange={e => setName(e.target.value)}
+                
               />
             </div>
 
@@ -213,12 +236,13 @@ import { supabase } from "../utils/supabase";
                   Email
                 </label>
                 <input
-                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md"
                   id="email"
                   type="email"
+                  placeholder={userProfile.email}
                   value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder="Email"
+                  onChange={e => setEmail(e.target.value)}
+                  
                 />
               </div>
               <div className="flex-1">
@@ -228,19 +252,14 @@ import { supabase } from "../utils/supabase";
                 >
                   Phone Number
                 </label>
-                <div className="flex mt-1 rounded-md shadow-sm">
-                  <span className="inline-flex items-center px-3 text-sm text-gray-500 border border-r-0 border-gray-300 rounded-l-md bg-gray-50">
-                    +966
-                  </span>
-                  <input
-                    type="text"
-                    className="flex-1 block w-full px-3 py-2 text-gray-700 bg-gray-300 rounded-none rounded-r-md"
-                    id="phone-number"
-                    value={phoneNumber}
-                    onChange={e => setPhoneNumber(e.target.value)}
-                    placeholder="5X XXX XXXX"
-                  />
-                </div>
+                <input
+                  type="text"
+                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 border-2 border-black rounded-md"
+                  id="phone-number"
+                  placeholder={userProfile.phoneNumber}
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
               </div>
             </div>
 
@@ -255,11 +274,10 @@ import { supabase } from "../utils/supabase";
                 </label>
                 <select
                   id="blood-type"
-                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
-                  value={bloodType}
-                  onChange={(e) => setBloodType(e.target.value)}
+                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md"
+                  value={userProfile.bloodType}
+                  disabled
                 >
-                  <option value="">Select Blood Type</option>
                   {bloodTypes.map((type) => (
                     <option key={type} value={type}>
                       {type}
@@ -275,12 +293,12 @@ import { supabase } from "../utils/supabase";
                   Weight (kg)
                 </label>
                 <input
-                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 border-2 border-black rounded-md"
                   id="weight"
                   type="number"
+                  placeholder={userProfile.weight.toString()}
                   value={weight}
-                  onChange={e => setWeight(e.target.valueAsNumber)}
-                  placeholder="Weight"
+                  onChange={(e) => setWeight(e.target.valueAsNumber)}
                 />
               </div>
             </div>
@@ -295,10 +313,9 @@ import { supabase } from "../utils/supabase";
                   Date of Birth
                 </label>
                 <input
-                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md"
                   id="date-of-birth"
                   type="text"
-                  max={getMaxDate()} // Prevent future dates
                   value={userProfile.dateOfBirth}
                   disabled
                 />
@@ -311,16 +328,16 @@ import { supabase } from "../utils/supabase";
                   Age
                 </label>
                 <input
-                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                  className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 rounded-md"
                   id="age"
                   type="text"
-                  value={age}
+                  value={userProfile.age}
                   disabled
                 />
               </div>
             </div>
 
-            {/* Address */}
+            {/* Email and Phone Number */}
             <div className="flex gap-4">
               <div className="flex-1">
                 <label
@@ -391,7 +408,6 @@ import { supabase } from "../utils/supabase";
               
             </div>
 
-            {/* Medical History */}
             <div>
               <label
                 className="block text-sm font-medium text-gray-700"
@@ -399,9 +415,15 @@ import { supabase } from "../utils/supabase";
               >
                 Medical History
               </label>
+              <label
+                className="block text-sm font-medium text-gray-700"
+                htmlFor="medical-history"
+              >
+                {userProfile.medicalHistory}
+              </label>
               <select
                 id="medical-history"
-                className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-300 rounded-md"
+                className="block w-full px-3 py-2 mt-1 text-gray-700 bg-gray-200 border-2 border-black rounded-md"
                 value={medicalHistory}
                 onChange={(e) => setMedicalHistory(e.target.value)}
               >
@@ -411,29 +433,29 @@ import { supabase } from "../utils/supabase";
                   </option>
                 ))}
               </select>
-              
             </div>
 
             {/* Button Container */}
-            <div className="flex items-center justify-center gap-4 ">
+            <div className="flex items-center justify-center gap-3">
               {" "}
               {/* Flex container with column direction */}
               {/* Change Password Button */}
-              <a
-                type="button"
-                className="bg-[#292828] text-white font-bold py-2 px-4 rounded cursor-pointer hover:bg-black transition-all duration-700 min-w-[120px]"
-                href="NewPasswordPage"
+              <Link
+                to="/NewPassword"
+                className=" cursor-pointer rounded-lg bg-[#292828] border-2 border-[#292828] px-3 py-2
+                text-base font-bold text-white align-middle transition-all duration-700 hover:bg-black focus:outline-none shadow-md hover:shadow-xl
+                  disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               >
                 Change Password
-              </a>
+              </Link>
               {/* Update Profile Button */}
-              <a
-                type="submit"
-                className="bg-[#292828] text-white font-bold py-2 px-4 rounded cursor-pointer hover:bg-black transition-all duration-700 min-w-[170px] text-center"
-                href="ShowProfilePage"
+              <button
+                className="cursor-pointer rounded-lg bg-[#292828] border-2 border-[#292828] px-7 py-2
+                text-base font-bold text-white align-middle transition-all duration-700 hover:bg-black focus:outline-none shadow-md hover:shadow-xl
+                  disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
               >
                 Save Changes
-              </a>
+              </button>
             </div>
           </form>
         </div>
